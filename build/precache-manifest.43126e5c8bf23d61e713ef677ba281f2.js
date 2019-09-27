ac081578e8b0bf4cbef44631c2f6fd34eb348d28
vaerilius <vaerilius@gmail.com>
2019-09-27 19:44:41 +0300

    test build

---
+++ build/precache-manifest.43126e5c8bf23d61e713ef677ba281f2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7171c367c98d0f6c98f86e39c617fc7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d300ad5383e7bb0918f8",
+    "url": "/static/js/2.74bd62dd.chunk.js"
+  },
+  {
+    "revision": "81867a82c99e2000a029",
+    "url": "/static/js/main.95b1a2b3.chunk.js"
+  },
+  {
+    "revision": "ea69f121b156bf480488",
+    "url": "/static/js/runtime-main.9c5e6e9a.js"
+  }
+]);

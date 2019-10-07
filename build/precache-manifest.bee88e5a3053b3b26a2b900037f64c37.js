4a8953f27ac8cc617d6bcb4dcd486e230f130868
vaerilius <tjohannest@gmail.com>
2019-10-07 11:21:57 +0300

    uibuild

---
+++ build/precache-manifest.bee88e5a3053b3b26a2b900037f64c37.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11d59c26db3702eefff493c8484c9183",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4396e5431db82d731039",
+    "url": "/static/js/2.9bfafbf7.chunk.js"
+  },
+  {
+    "revision": "3300e92f7f570ce2451d",
+    "url": "/static/js/main.1f82f513.chunk.js"
+  },
+  {
+    "revision": "ea69f121b156bf480488",
+    "url": "/static/js/runtime-main.9c5e6e9a.js"
+  }
+]);

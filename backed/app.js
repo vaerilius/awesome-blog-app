922663d716c78012dff2a8679f4531a4c60d5a31
Vaerilius <tjohannest@gmail.com>
2019-10-13 11:50:09 +0300

    uibuild

--- backed/app.js
+++ backed/app.js
@@ -10,6 +10,7 @@ const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 
+
 logger.info('connecting to', config.MONGODB_URI)
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })

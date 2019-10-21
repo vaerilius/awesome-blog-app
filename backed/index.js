b8d654e59c27aa3deaa614cbcb31f84211c7e6d3
vaerilius <tjohannest@gmail.com>
2019-10-21 10:48:40 +0300

    serverbuild

--- backed/index.js
+++ backed/index.js
@@ -1,20 +1,20 @@
 const app = require('./app')
 const http = require('http')
 const config = require('./utils/config')
-const socket = require('socket.io')
+// const socket = require('socket.io')
 
 const server = http.createServer(app)
 
-const io = socket(server)
+// const io = socket(server)
 
-io.on('connection', (socket) => {
-  console.log(socket.id);
+// io.on('connection', (socket) => {
+//   console.log(socket.id);
 
-  socket.on('SEND_MESSAGE', function(data){
-    io.emit('RECEIVE_MESSAGE', data);
-})
+//   socket.on('SEND_MESSAGE', function(data){
+//     io.emit('RECEIVE_MESSAGE', data);
+// })
 
-});
+// });
 
 
 server.listen(config.PORT, () => {

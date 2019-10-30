32afd445095f2ae4ee9d8a567a12a98b9b22a47d
vaerilius <tjohannest@gmail.com>
2019-10-30 10:34:13 +0200

    sort by blogs

--- frondend/src/services/blogs.service.js
+++ frondend/src/services/blogs.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
-// const baseUrl = 'https://stark-ocean-35945.herokuapp.com/api/blogs'
-const baseUrl = '/api/blogs'
+const baseUrl = 'https://stark-ocean-35945.herokuapp.com/api/blogs'
+// const baseUrl = '/api/blogs'
 
 let token = null
 
